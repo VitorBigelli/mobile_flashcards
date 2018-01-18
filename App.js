9eb0ae17a725bfb5275598d0c000c795bc3e5b78
Vitor Bigelli <vitorbigelli@hotmail.com>
2018-01-18 00:01:02 -0200

    feat: Create Decks API

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native'
+import { getDecks } from './utils/api'
 
 export default class App extends React.Component {
+
+  state = {
+    decks: {},
+  }
+
+  componentDidMount() {
+    getDecks()
+      .then( (data) => {
+        this.setState( (state) => ({
+          decks: data
+        }))
+      })
+  }
+
   render() {
+    const { decks } = this.state
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        { Object.keys(decks).map( (deck) => {
+              return (
+                <View key={decks[deck].title} >
+                  <Text> { decks[deck].title } </Text>
+                  <Text> { decks[deck].questions.length } questions </Text>
+                </View>
+              )
+            })
+        }
       </View>
-    );
+    )
   }
 }
 
@@ -19,5 +42,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
---
+++ utils/api.js
@@ -0,0 +1,61 @@
+import { AsyncStorage } from 'react-native'
+
+const DECKS_STORAGE_KEY = "MobileFlashcards:decks"
+
+const decks = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
+
+export function getDecks () { 
+
+	AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(decks))
+
+	return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+		.then( (data) => {
+			return JSON.parse(data)
+		})
+}
+
+export function getDeck(id) {
+	return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+		.then( (data) => {
+			return data[id]
+		})
+}
+
+export function saveDeckTitle(title) {
+	return AsyncStorage.mergeItem( DECKS_STORAGE_KEY, JSON.stringify({
+				[title]: { title: title, questions: [] }
+			}
+			))
+}
+
+export function addCardToDeck({ title, card }) {
+	return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+		.then( (results) => {
+			const { data } = JSON.parse(results)
+			data[title] = data[title].questions.push(card)
+			AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
+		})
+}
