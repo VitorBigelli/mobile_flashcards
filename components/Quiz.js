4ab679616dc3fc950c65a34626c835338cc5264a
Vitor Bigelli <vitorbigelli@hotmail.com>
2018-01-25 04:01:15 -0200

    feat: Modify Quiz UI

--- components/Quiz.js
+++ components/Quiz.js
@@ -8,6 +8,8 @@ import {
 import { getDeck } from '../utils/api'
 import { connect } from 'react-redux'
 import SubmitBtn from './SubmitBtn'
+import { red, lightGreen, lightRed, green } from '../utils/colors'
+
 
 class Quiz extends Component {
 
@@ -20,29 +22,91 @@ class Quiz extends Component {
 		const { deck } = this.props
 		const { currentQuestion, showAnswer } = this.state
 		return (
-			<View> 
-				<View> 
-					<Text> { currentQuestion } / {deck.questions.length} </Text>
+			<View style={styles.container} > 
+				<View style={styles.scoreContainer}> 
+					<Text style={styles.score}> { currentQuestion } / {deck.questions.length} </Text>
 				</View>
-				<Text> 
-					{ deck && deck.questions[currentQuestion].question }
-				</Text>
+				<View style={styles.questionContainer}>
+					<Text style={styles.question} > 
+						{ deck && deck.questions[currentQuestion].question }
+					</Text>
 
-				<TouchableOpacity> 
-					<Text> Answer </Text>
-				</TouchableOpacity>
+					<TouchableOpacity  style={styles.answerBtn} > 
+						<Text style={styles.answerBtnText} > Answer </Text>
+					</TouchableOpacity>
+				</View>
+				<View style={styles.buttonsContainer} >
+					<TouchableOpacity style={[styles.correctBtn, styles.button]} >
+						<Text style={styles.correctBtnText} > Correct </Text>
+					</TouchableOpacity>
 
-				<TouchableOpacity>
-					<Text> Correct </Text>
-				</TouchableOpacity>
-
-				<TouchableOpacity>
-					<Text> Incorrect </Text>
-				</TouchableOpacity>
+					<TouchableOpacity style={[styles.incorrectBtn, styles.button]} >
+						<Text style={styles.incorrectBtnText} > Incorrect </Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'stretch',
+	},
+	scoreContainer: {
+		alignSelf: 'flex-start',
+		height: 40,
+		width: 50,
+	}, 
+	score: {
+		fontSize: 18,
+	}, 
+	questionContainer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: 200,
+		marginTop: 50,
+	}, 
+	question: {
+		fontSize: 45,
+	},
+	buttonsContainer: {
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		height: 100,
+	},
+	button: {
+		height: 50, 
+		width: 150, 
+		borderRadius: 7, 
+		padding: 8,
+		marginBottom: 5,
+		alignItems: 'center',
+		justifyContent: 'center',
+	}, 
+	correctBtn: {
+		backgroundColor: green,
+	},
+	correctBtnText: {
+		fontSize: 15,
+		color: lightGreen,
+	},
+	incorrectBtn: {
+		backgroundColor: red,	}, 
+	incorrectBtnText: {
+		fontSize: 15,
+		color: lightRed,
+	}, 
+	answerBtn: {
+		alignItems: 'center',
+		justifyContent: 'center',
+	}, 
+	answerBtnText: {
+		fontSize: 15,
+		color: red,
+	},
+})
 
 function mapStateToProps(state, { navigation }) {
 	const { title } = navigation.state.params
--- utils/colors.js
+++ utils/colors.js
@@ -1,4 +1,7 @@
 export const white = '#fff'
-export const red = '#fa6565'
+export const red = '#801515'
 export const gray = '#333'
 export const lightGray = '#777'
+export const lightRed = '#ffaaaa'
+export const green = '#158015'
+export const lightGreen = '#aaffaa'
